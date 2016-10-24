965690e57af201dcdc634ef9167db29df0631f6c
Alex Corbi <a.corbi@gmail.com>
2016-10-24 13:50:24 -0400

    increased version number

--- wp-odm_profile_pages.php
+++ wp-odm_profile_pages.php
@@ -3,7 +3,7 @@
  * Plugin Name: ODM Profile Pages
  * Plugin URI: http://github.com/OpenDevelopmentMekong/odm_profile_pages
  * Description: Internal wordpress plugin for exposing custom content type for profile pages
- * Version: 2.0.2
+ * Version: 2.0.3
  * Author: Alex Corbi (mail@lifeformapps.com)
  * Author URI: http://www.lifeformapps.com
  * License: GPLv3.
