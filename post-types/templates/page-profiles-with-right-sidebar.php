6e0a2d6d27df44dda925833306ecdbb65706dd14
huyeng <huyeng.heng8@gmail.com>
2016-11-24 12:12:29 +0700

    Add map into the template

--- post-types/templates/page-profiles-with-right-sidebar.php
+++ post-types/templates/page-profiles-with-right-sidebar.php
@@ -1,9 +1,14 @@
 <section class="container">
     <div class="twelve columns post-title">
-			<section class="content section-content">
-				<section id="post-content">
-					<?php the_content(); ?>
-				</section>
+      <section class="content section-content">
+        <?php
+        if(function_exists('display_embedded_map')){
+          display_embedded_map(get_the_ID());
+        }
+        ?>
+        <section id="post-content">
+          <?php the_content(); ?>
+        </section>
       </section>
     </div>
 
