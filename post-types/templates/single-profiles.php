4bfe7829092cb886704f3f35988418da57527ac1
Alex Corbi <a.corbi@gmail.com>
2017-06-05 12:42:38 +0200

    Fixes #155

--- post-types/templates/single-profiles.php
+++ post-types/templates/single-profiles.php
@@ -39,24 +39,29 @@ $sub_navigation = get_post_meta($post->ID, '_page_with_sub_navigation', true);
 <?php if(!$sub_navigation):?>
 	<section class=	"container section-title main-title">
 	    <header class="row">
-	      <div class="ten columns">
+	      <div class="twelve columns">
 	        <h1><?php the_title(); ?></h1>
 	        <?php echo_post_meta(get_post()); ?>
 	      </div>
 	      <?php
-	      if(!empty($dataset)) { ?>
-	        <div class="six columns align-right">
+	      if(!empty($dataset)) :?>
+	        <div class="two columns align-right">
 	          <?php echo_download_button_link_to_datapage($ckan_dataset_id) ?>
 	        </div>
+          <div class="two columns">
+	          <div class="widget share-widget">
+	            <?php odm_get_template('social-share',array(),true); ?>
+	          </div>
+	        </div>
 	      <?php
-	      }else { ?>
+        else: ?>
 	        <div class="four columns">
 	          <div class="widget share-widget">
 	            <?php odm_get_template('social-share',array(),true); ?>
 	          </div>
 	        </div>
 	      <?php
-	      }
+      endif;
 	      ?>
 	    </header>
 	</section>
