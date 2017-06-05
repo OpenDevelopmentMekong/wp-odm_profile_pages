6c5a3a6f6af02ee3ec0580a04fd478a38e0131fd
Alex Corbi <a.corbi@gmail.com>
2017-06-05 15:23:10 +0200

    Fixes #155

--- post-types/templates/single-profiles.php
+++ post-types/templates/single-profiles.php
@@ -43,26 +43,16 @@ $sub_navigation = get_post_meta($post->ID, '_page_with_sub_navigation', true);
 	        <h1><?php the_title(); ?></h1>
 	        <?php echo_post_meta(get_post()); ?>
 	      </div>
-	      <?php
-	      if(!empty($dataset)) :?>
-	        <div class="four columns align-right">
-	          <?php echo_download_button_link_to_datapage($ckan_dataset_id) ?>
-	        </div>
-          <div class="four columns align-right">
-	          <div class="widget share-widget">
-	            <?php odm_get_template('social-share',array(),true); ?>
-	          </div>
-	        </div>
-	      <?php
-        else: ?>
-	        <div class="eight columns align-right">
-	          <div class="widget share-widget">
-	            <?php odm_get_template('social-share',array(),true); ?>
-	          </div>
-	        </div>
-	      <?php
-      endif;
-	      ?>
+        <div class="four columns align-right">
+          <div class="widget share-widget">
+            <?php odm_get_template('social-share',array(),true); ?>
+          </div>
+  	      <?php
+  	      if(!empty($dataset)) :?>	        
+            <?php echo_download_button_link_to_datapage($ckan_dataset_id) ?>           
+  	      <?php        
+          endif; ?>
+        </div>
 	    </header>
 	</section>
 <?php endif; ?>
