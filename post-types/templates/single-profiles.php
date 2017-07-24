05357b75be225f49ed850cf572cdf185ea880e97
Alex Corbi <a.corbi@gmail.com>
2017-07-24 18:08:16 +0200

    better title

--- post-types/templates/single-profiles.php
+++ post-types/templates/single-profiles.php
@@ -39,27 +39,27 @@ $sub_navigation = get_post_meta($post->ID, '_page_with_sub_navigation', true);
 <?php if(!$sub_navigation):?>
 
 	<section class=	"container section-title main-title">
-	    <header class="row">
-	      <div class="ten columns">
-	        <h1><?php the_title(); ?></h1>
-        	<?php echo_post_meta(get_post()); ?>
-	      </div>
-	      <?php
-	      if(!empty($dataset)): ?>
-	        <div class="six columns align-right">
-	          <?php echo_download_button_link_to_datapage($ckan_dataset_id) ?>
-	        </div>
-	      <?php
-				elseif (odm_screen_manager()->is_desktop()): ?>
-	        <div class="four columns">
-	          <div class="widget share-widget">
-	            <?php odm_get_template('social-share',array(),true); ?>
-	          </div>
-	        </div>
-	      <?php
-	    	endif;
-	      ?>
-	    </header>
+    <header class="row">
+      <div class="ten columns">
+        <h1><?php the_title(); ?></h1>
+      	<?php echo_post_meta(get_post()); ?>
+      </div>
+      <?php
+      if(!empty($dataset)): ?>
+        <div class="six columns align-right">
+          <?php echo_download_button_link_to_datapage($ckan_dataset_id) ?>
+        </div>
+      <?php
+			elseif (odm_screen_manager()->is_desktop()): ?>
+        <div class="four columns">
+          <div class="widget share-widget">
+            <?php odm_get_template('social-share',array(),true); ?>
+          </div>
+        </div>
+      <?php
+    	endif;
+      ?>
+    </header>
 	</section>
 <?php endif; ?>
 
