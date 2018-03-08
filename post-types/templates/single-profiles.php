c58ed911cc1d5af7333a3317f3cf73a202f8954a
huyeng <huyeng.heng8@gmail.com>
2018-03-08 22:44:33 +0700

    Update styling of download button

--- post-types/templates/single-profiles.php
+++ post-types/templates/single-profiles.php
@@ -40,20 +40,20 @@ $sub_navigation = get_post_meta($post->ID, '_page_with_sub_navigation', true);
 <?php if(!$sub_navigation):?>
 	<section class=	"container section-title main-title">
 	    <header class="row">
-	      <div class="nine columns">
+	      <div class="twelve columns">
 	        <h1><?php the_title(); ?></h1>
 	        <?php echo_post_meta(get_post()); ?>
 	      </div>
-        <div class="seven columns align-right">
+        <div class="four columns align-right">
           <div class="widget share-widget">
-						<div class="eight columns">
+						<div class="four columns">
 							<?php
 		  	      if(!empty($dataset)) :?>
 		            <?php echo_download_button_link_to_datapage($ckan_dataset_id) ?>
 		  	      <?php
 		          endif; ?>
 						</div>
-						<div class="eight columns">
+						<div class="twelve columns">
 	            <?php odm_get_template('social-share',array(),true); ?>
 						</div>
           </div>
